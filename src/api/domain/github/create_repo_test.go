f6a5034a8c25289b9d5df3fc5a70947c8cce80d1
matias <matias@justdice.io>
2022-07-25 12:25:33 +0200

    Adding github create repo api

---
+++ src/api/domain/github/create_repo_test.go
@@ -0,0 +1,32 @@
+package github
+
+import (
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCreateRepoRequestAsJson(t *testing.T) {
+	request := CreateRepoRequest{
+		Name:        "golang introduction",
+		Description: "a golang introduction repo",
+		Homepage:    "https://github.com",
+		Private:     true,
+		HasIssues:   true,
+		HasProjects: true,
+		HasWiki:     true,
+	}
+
+	bytes, err := json.Marshal(request)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, bytes)
+
+	var target CreateRepoRequest
+
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
+
+	assert.EqualValues(t, target.Name, request.Name)
+	assert.EqualValues(t, target.HasIssues, request.HasIssues)
+}
